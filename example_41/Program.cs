a6aa951e83e23d3fe148e595d0521336d55f9167
unknown <Ivan>
2023-04-28 13:26:25 +0700

    done example_41

--- example_41/Program.cs
+++ example_41/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+Console.Write("Введите размер массива --> ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] createArray = CreateArray(size);
+PrintArray(createArray);
+int howNumberMoreZero = HowNumberMoreZero(createArray);
+Console.WriteLine();
+Console.WriteLine($"Чисел больше 0 = {howNumberMoreZero}");
+
+int[] CreateArray(int length)
+{
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int HowNumberMoreZero(int[] arr)
+{
+    int number = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) number++;
+    }
+    return number;
+}
